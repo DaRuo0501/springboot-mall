d71cdb0e4aba4d2579cb2593f7dca7329c446ade
DaRuo <jo896413@gmail.com>
2023-03-04 14:09:17 +0800

    初始化 Spring Boot Project

---
+++ springboor-mall/src/main/java/com/daruo/springboormall/SpringboorMallApplication.java
@@ -0,0 +1,13 @@
+package com.daruo.springboormall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboorMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboorMallApplication.class, args);
+    }
+
+}
---
+++ springboor-mall/src/test/java/com/daruo/springboormall/SpringboorMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daruo.springboormall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboorMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
